f896228882803f317ed1722c02b5b64c04489e1a
aonly <atiffiany@outlook.com>
2022-06-17 23:05:33 +0800

    feat:finish index

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-2g89sppo64cc67db","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Program\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-2g89sppo64cc67db --n quickstartFunctions --r --project "D:\ProjectsQin\front_end_projects\WeChatProjects\music" --report_first --report
